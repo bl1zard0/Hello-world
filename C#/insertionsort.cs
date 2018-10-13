0268fb6946b32c65996460eea7ac5e381d324587
bl1zard0 <41521340+bl1zard0@users.noreply.github.com>
2018-10-13 09:57:48 +0200

    insertionsort
    in C#

---
+++ C#/insertionsort.cs
@@ -0,0 +1,18 @@
+﻿//Insertion sort
+
+public static void insertionSort(int[] list)
+{
+    int item, j;
+    for (int i = 1; i <= (list.Length - 1); i++)
+    {
+        // saving 
+        item = list[i];
+        j = i - 1;
+        while ((j >= 0) && (list[j] > item))
+        {
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = item;
+    }
+}
